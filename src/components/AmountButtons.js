8b507f17ac14bf77cf691fe075251b39daf8f5fd
69is54 <chienliko66@gmail.com>
2024-03-09 10:25:37 +0800

    fix: style

--- src/components/AmountButtons.js
+++ src/components/AmountButtons.js
@@ -1,21 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
 import { FaPlus, FaMinus } from 'react-icons/fa'
-
-const AmountButtons = ({ increase, decrease, amount }) => {
-  return (
-    <Wrapper className='amount-btns'>
-      <button type='button' className='amount-btn' onClick={decrease}>
-        <FaMinus />
-      </button>
-      <h2 className='amount'>{amount}</h2>
-      <button type='button' className='amount-btn' onClick={increase}>
-        <FaPlus />
-      </button>
-    </Wrapper>
-  )
-}
-
 const Wrapper = styled.div`
   display: grid;
   width: 140px;
@@ -40,5 +25,19 @@ const Wrapper = styled.div`
     margin-bottom: 0;
   }
 `
+
+const AmountButtons = ({ increase, decrease, amount }) => {
+  return (
+    <Wrapper className='amount-btns'>
+      <button type='button' className='amount-btn' onClick={decrease}>
+        <FaMinus />
+      </button>
+      <h2 className='amount'>{amount}</h2>
+      <button type='button' className='amount-btn' onClick={increase}>
+        <FaPlus />
+      </button>
+    </Wrapper>
+  )
+}
 
 export default AmountButtons
